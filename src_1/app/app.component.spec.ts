2f3d3080357dbe7131652b976bb4dd30b84450bd
Jaganjaggu <jaganvofficial@gmail.com>
2023-12-17 22:27:56 +0530

    First Commit

--- src_1/app/app.component.spec.ts
+++ src_1/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proBuy'`, () => {
+  it(`should have as title 'angular-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proBuy');
+    expect(app.title).toEqual('angular-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proBuy app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-project app is running!');
   });
 });
